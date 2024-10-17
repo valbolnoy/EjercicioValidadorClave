f1c4ee077c1f0bf4a0567e7fc775cf7e7c21670b
valnolboy <valeriabolnoy@gamil.com>
2024-10-17 11:42:24 -0500

    Prueba 3

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -11,3 +11,9 @@ class ReglaValidacion(ABC):
     def _validar_longitud(self, clave: str) -> bool:
         return len(clave) > self._longitud_esperada
 
+    @staticmethod
+    def _contiene_mayuscula(clave: str) -> bool:
+        for caracter in clave:
+            if caracter.isupper():
+                return True
+        return False
